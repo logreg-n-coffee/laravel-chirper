3ee7cf3089dfdeba8824be19741eefc085920c03
Rui Hu <8199515+logreg-n-coffee@users.noreply.github.com>
2023-05-16 14:54:15 -0700

    Add editing chirps

--- resources/js/Components/Chirp.jsx
+++ resources/js/Components/Chirp.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-// import dayjs to generate relative date
+// Import components
+import Dropdown from '@/Components/Dropdown';
+import InputError from '@/Components/InputError';
+import PrimaryButton from '@/Components/PrimaryButton';
+
+// Import Inertia
+import { useForm, usePage } from '@inertiajs/react';
+
+// Import dayjs to generate relative date
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import isBetween from 'dayjs/plugin/isBetween';
@@ -10,9 +18,31 @@ dayjs.extend(relativeTime);
 dayjs.extend(isBetween);
 
 export default function Chirp({ chirp }) {
+    // Define constants for dayjs
     const now = dayjs();
     const threeDaysAgo = now.subtract(3, 'day');
 
+    // Editing Chirps
+    const { auth } = usePage().props;
+
+    // Set editing to false by default
+    const [editing, setEditing] = useState(false);
+
+    // Use the useForm hook to manage form data
+    const { data, setData, patch, clearErrors, reset, errors } = useForm({
+        // Initialize the form with the chirp message
+        message: chirp.message,
+    });
+
+    // When the form is submitted, send a PATCH request to the server
+    const submit = (e) => {
+        e.preventDefault();
+
+        // When the request is successful, set editing to false
+        patch(route('chirps.update', chirp.id), { onSuccess: () => setEditing(false) });
+    };
+
+    // Render the Chirp
     return (
         <div className="p-6 flex space-x-2">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-600 -scale-x-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
@@ -28,7 +58,34 @@ export default function Chirp({ chirp }) {
                         </small>
                     </div>
                 </div>
-                <p className="mt-4 text-lg text-gray-900">{chirp.message}</p>
+                {/* Dropdown will be rendered only when the current user is the user signed in */}
+                {chirp.user.id === auth.user.id &&
+                    <Dropdown>
+                        <Dropdown.Trigger>
+                            <button>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                    <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                                </svg>
+                            </button>
+                        </Dropdown.Trigger>
+                        <Dropdown.Content>
+                            <button className="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:bg-gray-100 transition duration-150 ease-in-out" onClick={() => setEditing(true)}>
+                                Edit
+                            </button>
+                        </Dropdown.Content>
+                    </Dropdown>
+                }
+                {editing
+                    ? <form onSubmit={submit}>
+                        <textarea value={data.message} onChange={e => setData('message', e.target.value)} className="mt-4 w-full text-gray-900 border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"></textarea>
+                        <InputError message={errors.message} className="mt-2" />
+                        <div className="space-x-2">
+                            <PrimaryButton className="mt-4">Save</PrimaryButton>
+                            <button className="mt-4" onClick={() => { setEditing(false); reset(); clearErrors(); }}>Cancel</button>
+                        </div>
+                    </form>
+                    : <p className="mt-4 text-lg text-gray-900">{chirp.message}</p>
+                }
             </div>
         </div>
     );
